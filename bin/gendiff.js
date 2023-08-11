438992bfd63665365fac85c93ee6ee21102834bc
Grigory Tydykov <grigory@H410M-H-V3-7390121d.localdomain>
2023-08-11 15:24:46 +0700

    add in cli arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,13 @@
 import { program } from 'commander';
 
 program
-  .name('generator diff')
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .helpOption('-h --help', 'output usage information')
   .version('0.1.0', '-V, --version', 'output the version number');
 
+program
+  .option('-f --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
