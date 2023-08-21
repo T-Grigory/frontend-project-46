4e2d689d36a3003c4761685ea7e3c501861d47b2
Grigory Tydykov <grigory@H410M-H-V3-7390121d.localdomain>
2023-08-21 16:44:15 +0700

    add linter

--- src/gendiff.js
+++ src/gendiff.js
@@ -16,22 +16,10 @@ const gendiff = (filepath1, filepath2) => {
   const data2 = getNormalizeData(absoluteFilePath2);
   const keys = _.sortBy(_.uniq([...Object.keys(data1), ...Object.keys(data2)]));
 
-  // const formatedData = keys.map((key) => {
-  //   if (data1.hasOwnProperty(key) && !data2.hasOwnProperty(key)) {
-  //     return `- ${key}: ${data1[key]}`;
-  //   } else if (!data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) {
-  //     return `+ ${key}: ${data2[key]}`;
-  //   } else if (data1[key] !== data2[key]) {
-  //     return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
-  //   } else {
-  //     return `  ${key}: ${data1[key]}`;
-  //   }
-  // });
-
   const formattedData = keys.reduce((accumulator, key) => {
-    if (data1.hasOwnProperty(key) && !data2.hasOwnProperty(key)) {
+    if (_.has(data1, key) && !_.has(data2, key)) {
       accumulator.push(`- ${key}: ${data1[key]}`);
-    } else if (!data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) {
+    } else if (!_.has(data1, key) && _.has(data2, key)) {
       accumulator.push(`+ ${key}: ${data2[key]}`);
     } else if (data1[key] !== data2[key]) {
       accumulator.push(`- ${key}: ${data1[key]}`);
